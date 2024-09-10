65ff6275cd56f9e19acf560650ba24335dbf1232
najwa-shahbaz-920 <najwashahbaz920@gmail.com>
2024-09-10 23:18:44 +0500

    Find weather the number is Even or Odd

---
+++ EvenOdd.py
@@ -0,0 +1,13 @@
+ch=int(input("Enter any number\n 1-Even\n 2-Odd\n"))
+if ch==1:
+    print("\nEven Numbers: ")
+    for a in range(0,10,2):
+        print(a)
+elif ch==2:
+    print("\nOdd Numbers: ")
+    for a in range(1,10,2):
+        print(a)
+
+
+
+
