74d8a092f4e3384aa34676da7c0923d57ddc6a9f
najwa-shahbaz-920 <najwashahbaz920@gmail.com>
2024-09-10 23:19:52 +0500

    Find weather the number is Positive or Nagitive

---
+++ positiveNAgitive.py
@@ -0,0 +1,11 @@
+a=1
+while a==1:
+    num=int(input("Enter any Number: "))
+    if num>0:
+        print("Number is Positive")
+    elif num<0:
+        print("Number is Nagitive")
+    else:
+        print("Number is Zero")
+    a=int(input("Do you want to Run Again enter 1: "))
+    
