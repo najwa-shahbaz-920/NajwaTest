d93e6e80d3c005e998e722f09923133d3b0884e9
najwa-shahbaz-920 <najwashahbaz920@gmail.com>
2024-09-10 23:14:07 +0500

    Slicing the message

---
+++ Slicing.py
@@ -0,0 +1,12 @@
+s="I like to eat fast food"
+
+# starting & ending number define
+print(s[1:10])   
+# only ending number define
+print(s[:10])
+# only starting number define
+print(s[10:])
+# only step define
+print(s[::2])
+# starting ,ending and step define
+print(s[0:20:5])
