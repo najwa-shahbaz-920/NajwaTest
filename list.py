e233871e6a9a288bf23a3f64d1db2c3ab4a16dcd
najwa-shahbaz-920 <najwashahbaz920@gmail.com>
2024-09-11 20:41:02 +0500

    check the list functions

---
+++ list.py
@@ -0,0 +1,12 @@
+a=["Apple","Orange","Mango","Banana"]
+# to add the itemm in end
+a.append("Kiwi")
+# to sort the array
+a.sort(reverse=True)
+# to remove the 
+a.remove("Apple")
+a.extend("pineApple")
+a.insert(2,"graphs")
+a.reverse()
+for x in a:
+    print(x) 
