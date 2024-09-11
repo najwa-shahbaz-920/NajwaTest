1c7f1b81127e7ea330feecb91541bfe955bea239
najwa-shahbaz-920 <najwashahbaz920@gmail.com>
2024-09-11 20:43:15 +0500

    how to print in python

---
+++ print.py
@@ -0,0 +1,5 @@
+print("My name is najwa")
+print("I am 22 years old")
+print("I live in Islamabad")
+print("I am currently doing BSCS from Riphah International University Islamabad")
+print("My hobby is to Exploring New Things")
